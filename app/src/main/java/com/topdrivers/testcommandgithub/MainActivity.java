3c72fbe804bca7cc3b0493f278a9a5b5fd2fc500
omari <smail.omari@lapost.net>
2022-03-07 13:48:10 +0100

    troisième étape

--- app/src/main/java/com/topdrivers/testcommandgithub/MainActivity.java
+++ app/src/main/java/com/topdrivers/testcommandgithub/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //première étape
         //deuxième étape
+        //troisième étape
     }
 }
