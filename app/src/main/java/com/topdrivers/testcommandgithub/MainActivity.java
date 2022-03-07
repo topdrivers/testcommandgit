217e93b54668fb7a4677dee0ac5529704ff8eb3a
omari <smail.omari@lapost.net>
2022-03-07 08:15:10 +0100

    première étape

---
+++ app/src/main/java/com/topdrivers/testcommandgithub/MainActivity.java
@@ -0,0 +1,15 @@
+package com.topdrivers.testcommandgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //première étape
+    }
+}
